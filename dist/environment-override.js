b277b96db9981bfbf8858b393b80af5cfac9d1a6
Alexandru Badiu <andu@ctrlz.ro>
2018-01-10 15:39:23 +0200

    Build 1.0.3.

--- dist/environment-override.js
+++ dist/environment-override.js
@@ -42,6 +42,9 @@ function override(values, prefix, show) {
     // Replace double underscores.
     keyPrefix = keyPrefix.replace(/__/g, '_');
 
+    // Replace slashes.
+    keyPrefix = keyPrefix.replace(/\//g, '_');
+
     if (process.env[keyPrefix] !== undefined) {
       if (process.env[keyPrefix] === 'OVERRIDE_REMOVE_DATA') {
         delete manifest[key];
